c28462105bdfee349db3171cafa612fd5b261d6f
Nitish Kumar <nitishgupta784@gmail.com>
2024-09-03 18:20:49 +0530

    Create displayData.js

---
+++ displayData.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class DisplayData extends LightningElement {
+
+    name = 'John';
+    company = 'Salesforce';
+    designation = 'Manager';
+    salary = '$100000';
+}
