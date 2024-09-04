f0f02cfdef4b5fd1ed5cf26a2feddffbf9dc3590
Nitish Kumar <nitishgupta784@gmail.com>
2024-09-04 13:04:44 +0530

    Create displayData2.js

---
+++ displayData2.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class DisplayData2 extends LightningElement {
+
+    name = 'Nitish';
+    company = 'salesforce';
+    designation = 'salesforce developer';
+    salary = '$100000';
+}
